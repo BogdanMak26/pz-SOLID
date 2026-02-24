819e80fe50c057f1aab93b1f385bf992a3e498b6
BogdanMak26 <bogdan.mak123@gmail.com>
2026-02-24 14:59:56 +0200

    done Makarenko

--- src/Liskov/liskov_good.ts
+++ src/Liskov/liskov_good.ts
@@ -1,30 +1,20 @@
 class Order {
     id: number;
     items: string[];
-
-    // constructor
 }
-
 class ConfirmedOrder extends Order {
     payed: boolean;
-
-    // constructor
-
     markAsPaid(): void {
         this.payed = true;
     }
 }
 class DraftOrder extends Order {
-
 }
-
-
 
 const order = new ConfirmedOrder();
 const order1 = new Order();
 const order2 = new Order();
 const draftorder = new DraftOrder();
-
 
 //ми не можемо засунути в масив ніякі об'єкти окрім ConfirmedOrder
 const mas: ConfirmedOrder[] = [order,draftorder, order2, order1];
